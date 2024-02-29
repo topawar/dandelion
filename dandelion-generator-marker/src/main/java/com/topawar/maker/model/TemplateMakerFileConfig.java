c11c4fc531c7590fd79788f311056ea5f1d9e474
topawar <yangjun0125@yeah.net>
2024-03-01 01:00:46 +0800

    文件分组，bug修复

---
+++ dandelion-generator-marker/src/main/java/com/topawar/maker/model/TemplateMakerFileConfig.java
@@ -0,0 +1,55 @@
+package com.topawar.maker.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @author topawar
+ */
+@Data
+public class TemplateMakerFileConfig {
+
+    private List<FileInfoConfig> fileInfoConfigList;
+
+    /**
+     * 分组
+     */
+    private FileGroupConfig fileGroupConfig;
+
+    @Data
+    @NoArgsConstructor
+    public static class FileInfoConfig{
+        /**
+         * 路径
+         */
+        private String path;
+
+        /**
+         * 过滤规则列表
+         */
+        private List<FileFilterConfig> fileFilterConfigList;
+    }
+
+    /**
+     * 文件分组，对应meta.json
+     */
+    @Data
+    @NoArgsConstructor
+    public static class FileGroupConfig {
+        /**
+         * 分组的key
+         */
+        private String groupKey;
+        /**
+         * 名称
+         */
+        private String groupName;
+
+        /**
+         * 条件
+         */
+        private String condition;
+    }
+}
